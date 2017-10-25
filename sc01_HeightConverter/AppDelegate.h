0f8cf53940d3720c236b865889c5527ccd7cee37
user <user@W347439.local>
2017-10-25 19:21:06 -0400

    Initial Commit

---
+++ sc01_HeightConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sc01_HeightConverter
+//
+//  Created by user on 10/25/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sc01_HeightConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sc01_HeightConverter
+//
+//  Created by user on 10/25/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
